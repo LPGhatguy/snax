82b8b26a9597500e655bda653c9e6afd6a906921
Lucien Greathouse <me@lpghatguy.com>
2019-02-18 20:42:40 -0800

    snax_macros -> snax_impl

--- snax/src/lib.rs
+++ snax/src/lib.rs
@@ -1,32 +1,32 @@
 //! Snax is an implementation of a JSX-like grammar for Rust. You can use it as
 //! part of the `snax` crate, which provides a complete, untyped DOM that's
 //! quick to turn into a string.
-//! 
+//!
 //! If you're a proc macro author, you can also consume the `snax_syntax` to
 //! produce whatever structures you need for your project. This can be used to
 //! support a typed DOM or any React-like framework!
-//! 
+//!
 //! ## Installation
 //! Snax requires Rust 1.32 or newer.
-//! 
+//!
 //! It isn't published to crates.io yet, but you can use a Git dependency:
-//! 
+//!
 //! ```toml
 //! snax = { git = "https://github.com/LPGhatguy/snax.git" }
 //! ```
-//! 
+//!
 //! Some things are still a bit in flux, so I'm sorry in advance if I break
 //! anything!
-//! 
+//!
 //! ## Examples
-//! 
+//!
 //! ### Simple Page
 //! ```rust
 //! use snax::snax;
-//! 
+//!
 //! fn main() {
 //!     let page_title = "Hello, world, from Snax!";
-//! 
+//!
 //!     let page = snax! {
 //!         /* Snax supports regular multi-line Rust comments. */
 //!         <html>
@@ -50,20 +50,20 @@
 //!             </body>
 //!         </html>
 //!     };
-//! 
+//!
 //!     // The result of the snax! macro is HtmlContent.
 //!     // It implements Display and gives you compact HTML without a doctype!
 //!     println!("<!doctype html>");
 //!     println!("{}", page);
 //! }
 //! ```
-//! 
+//!
 //! ### Composition via functions
 //! Snax is designed to work well when using functions to reuse pieces of HTML!
-//! 
+//!
 //! ```rust
 //! use snax::{snax, Fragment, HtmlContent};
-//! 
+//!
 //! fn user_widget<'a>(name: &'a str, age: u32) -> HtmlContent<'a> {
 //!     snax! {
 //!         <div class="user">
@@ -71,14 +71,14 @@
 //!         </div>
 //!     }
 //! }
-//! 
+//!
 //! fn users() -> HtmlContent<'static> {
 //!     let users = vec![
 //!         ("Gandalf", 34),
 //!         ("Arwen Undómie", 75),
 //!         ("Primula Brandybuck", 133),
 //!     ];
-//! 
+//!
 //!     snax! {
 //!         <div class="users">
 //!             { Fragment::new(users.iter().map(|(name, age)| user_widget(name, *age))) }
@@ -86,7 +86,7 @@
 //!     }
 //! }
 //! ```
-//! 
+//!
 //! ## License
 //! Snax is available under the MIT license. See [LICENSE.txt](LICENSE.txt) for
 //! details.
@@ -96,5 +96,5 @@ mod types;
 use proc_macro_hack::proc_macro_hack;
 
 #[proc_macro_hack(support_nested)]
-pub use snax_macros::snax;
+pub use snax_impl::snax;
 pub use types::*;
--- snax_macros/README.md
+++
@@ -1,5 +0,0 @@
-# snax_macros
-snax_macros is the proc macro implementation crate used by [snax][snax]. It uses the [snax_syntax][snax_syntax] crate to parse JSX-like syntax and turn it into a virtual DOM.
-
-[snax]: https://crates.io/crates/snax
-[snax_syntax]: https://crates.io/crates/snax_syntax
